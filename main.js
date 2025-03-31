e922ce1527654d749b655ef8d5495239260da8b6
CAIO LOPES DURAES <caio.lduraes@senacsp.edu.br>
2025-03-31 20:10:56 -0300

    Novo Projeto

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-console.log("Electron - Processo principal")
+/**
+ * 
+ * 
+ */
 
-// importação dos recursos do framework
+console.log("Hello Electron! Processo Principal.")
+
+
+// FINAL STARTER CODE  - Importação dos recursos do framework
 // app (aplicação)
 // BrowserWindow (criação da janela)
 // nativeTheme (definir tema claro ou escuro)
-// Menu (definir um menu personalizado)
+// Menu (Definir um menu personalizado)
 // shell (acessar links externos no navegador padrão)
-// ipcMain (permite estabelecer uma comunicação entre processos (IPC) main.js <=> renderer.js)
+// ipcMain permite estabelecer uma comunicação entre processos (IPC) mains.js - renderer.js
 const { app, BrowserWindow, nativeTheme, Menu, shell, ipcMain } = require('electron/main')
 
 // Ativação do preload.js (importação do path)
 const path = require('node:path')
 
-// Importação dos métodos conectar e desconectar (módulo de conexão)
+//  Importação dos métodos conectar e desconectar (módulo de conexão)
 const { conectar, desconectar } = require('./database.js')
 
 // Importação do modelo de dados (Notes.js)
@@ -21,94 +27,114 @@ const noteModel = require('./src/models/Notes.js')
 // Janela principal
 let win
 const createWindow = () => {
-  // definindo o tema da janela claro ou ecuro
+  // definindo o tema da janela claro ou escuro
   nativeTheme.themeSource = 'light'
   win = new BrowserWindow({
     width: 1010,
     height: 720,
-    //frame: false,
-    //resizable: false,
-    //minimizable: false,
-    //closable: false,
-    //autoHideMenuBar: true,
+    // frame = menu padrão do electro, da p tirar e fazer na mão p personalizar com cores fora do padrão (preto/branco)
+    // frame: false
+    // 
+    // resizable: false,
+    // minimizable: false,
+    // closable: false,
+    // autoHideMenuBar: true
+
+    // Preload
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // Carregar o menu personalizado
-  // Atenção! Antes importar o recurso Menu
+  // ATENÇÃO: Antes importar o recurso Menu
   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-  // carregar o documento html na janela
+  // Carregar o documento html na janela
   win.loadFile('./src/views/index.html')
 }
 
-// janela sobre
+// Janela Sobre
 let about
 function aboutWindow() {
   nativeTheme.themeSource = 'light'
-  // obter a janela principal
+
+  // Obter a janela principal
   const mainWindow = BrowserWindow.getFocusedWindow()
-  // validação (se existir a janela principal)
+
+  //validação (se existir a janela principal)
   if (mainWindow) {
+
     about = new BrowserWindow({
       width: 300,
       height: 200,
       autoHideMenuBar: true,
       resizable: false,
       minimizable: false,
-      // estabelecer uma relação hierárquica entre janelas
+      // Estabelecer uma relação hierarquica entre janelas
       parent: mainWindow,
-      // criar uma janela modal (só retorna a principal quando encerrada)
+      // Criar uma janela modal (só retorna a principal quando encerrada)
       modal: true,
+      // Com essa linha estou vinculando a janela Sobre com o preload.js
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
       }
+
     })
   }
-
   about.loadFile('./src/views/sobre.html')
 
-  //recebimento da mensagem do renderizador da tela sobre para fechar a janela usando o botão OK
+  // Recebimento da mensagem do renderizador da tela sobre para fechar  a janela usando o botão
   ipcMain.on('about-exit', () => {
-    //validação (se existir a janela e ela não estiver sido destruída, fechar)
+    // Validação: se existir a janela e ela não estiver destruída, fechar
     if (about && !about.isDestroyed()) {
       about.close()
-    }   
+    }
   })
 }
 
-// janela nota
+// Janela Nota
 let note
 function noteWindow() {
   nativeTheme.themeSource = 'light'
-  // obter a janela principal
+
+  // Obter a janela principal
   const mainWindow = BrowserWindow.getFocusedWindow()
-  // validação (se existir a janela principal)
+
+  //validação (se existir a janela principal)
   if (mainWindow) {
+
     note = new BrowserWindow({
       width: 400,
-      height: 270,
+      height: 300,
       autoHideMenuBar: true,
-     // resizable: true,
-     // minimizable: false,
-      // estabelecer uma relação hierárquica entre janelas
+      //resizable: false,
+      //minimizable: false,
+      // Estabelecer uma relação hierarquica entre janelas
       parent: mainWindow,
-      // criar uma janela modal (só retorna a principal quando encerrada)
+      // Criar uma janela modal (só retorna a principal quando encerrada)
       modal: true,
+      // Com essa linha estou vinculando a janela Sobre com o preload.js
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
       }
+
     })
   }
-
   note.loadFile('./src/views/nota.html')
 
-
+  //Talvez utilizar no botão cancelar
+  /** 
+    // Recebimento da mensagem do renderizador da tela sobre para fechar  a janela usando o botão
+    ipcMain.on('about-exit', () =>{
+      // Validação: se existir a janela e ela não estiver destruída, fechar
+      if(note && !note.isDestroyed()){
+        note.close()
+      }
+    }) */
 }
 
-// inicialização da aplicação (assíncronismo)
+// inicialização da aplicação (assincronismo)
 app.whenReady().then(() => {
   createWindow()
 
@@ -117,19 +143,18 @@ app.whenReady().then(() => {
   // ipcMain.on (receber mensagem)
   // db-connect (rótulo da mensagem)
   ipcMain.on('db-connect', async (event) => {
-    //a linha abaixo estabelece a conexão com o banco de dados e verifica se foi conectado com sucesso (return true)
-    const conectado = await conectar()
-    if (conectado) {
-      // enviar ao renderizador uma mensagem para trocar a imagem do ícone do status do banco de dados (criar um delay de 0.5 ou 1s para sincronização com a nuvem)
-      setTimeout(() => {
-        // enviar ao renderizador a mensagem "conectado"
-        // db-status (IPC - comunicação entre processos - preload.js)
-        event.reply('db-status', "conectado")
-      }, 500) //500ms = 0.5s
-    }
+    // a linha abaixo estabelece a conexão com o banco de dados
+    await conectar()
+    // enviar ao renderizador uma mensagem para trocar a imagem do icone do status do banco de dados (criar um delay de 0.5 ou 1s para sincronização com a nuvem)
+    setTimeout(() => {
+      // Enviar ao renderizador a mensagem "conectado"
+      // db-status (IPC - Comunicaçao entre processos - preload.js)
+      event.reply('db-status', "conectado")
+    }, 500); // 500ms = 0.5s
+
   })
 
-  // só ativar a janela principal se nenhuma outra estiver ativa
+  // Só ativar a janela principal se nenuhma outra estiver ativa
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
@@ -137,29 +162,34 @@ app.whenReady().then(() => {
   })
 })
 
-// se o sistem não for MAC encerrar a aplicação quando a janela for fechada
+
+// Se o sistema não for MAC encerrar a aplicação quando a janela for fechada
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-// IMPORTANTE! Desconectar do banco de dados quando a aplicação for finalizada
+// IMPORTANTE!!! Desconectar do banco de dados quando a aplicação for finalizada
 app.on('before-quit', async () => {
   await desconectar()
 })
 
-// Reduzir a verbosidade de logs não críticos (devtools)
+// Reduzir a verbozidade de logs não críticos (devtools)
 app.commandLine.appendSwitch('log-level', '3')
 
-// template do menu
+// ================================ 
+
+// Template do menu
 const template = [
   {
     label: 'Notas',
     submenu: [
       {
         label: 'Criar nota',
+        // tecla de atalho
         accelerator: 'Ctrl+N',
+        // "função"
         click: () => noteWindow()
       },
       {
@@ -176,11 +206,12 @@ const template = [
     label: 'Ferramentas',
     submenu: [
       {
-        label: 'Aplicar zoom',
+        label: 'Aplica zoom',
         role: 'zoomIn'
       },
       {
-        label: 'Reduzir',
+        // verificar funcionalidade com professor
+        label: 'Reduzir zoom',
         role: 'zoomOut'
       },
       {
@@ -196,7 +227,7 @@ const template = [
       },
       {
         label: 'DevTools',
-        role: 'toggleDevTools'
+        role: 'ToggleDevTools'
       }
     ]
   },
@@ -205,7 +236,7 @@ const template = [
     submenu: [
       {
         label: 'Repositório',
-        click: () => shell.openExternal('https://github.com/professorjosedeassis/stickynotes')
+        click: () => shell.openExternal('https://github.com/vitorapassos/stickynotes.git')
       },
       {
         label: 'Sobre',
@@ -215,21 +246,27 @@ const template = [
   }
 ]
 
-// ==================================================================================================
-// == CRUD Create - Inicio ==========================================================================
 
-// Recebimento do objeto que contém os dados da nota
+// ===============================================
+// ================ CRUD CREATE ==================
+
+// Recebimento do objeto que contem os dados da Nota
+
 ipcMain.on('create-note', async (event, stickyNote) => {
-  // IMPORTANTE! Teste de recebimento do objeto - Passo 2
+  // IMPORTANTE!!! Teste de recebimento do objeto ( Passo 2)
   console.log(stickyNote)
-  //Criar uma nova estrutura  de dados para salvar no banco
-  //Atenção! os atributosd da estrutura precisam ser idênticos ao modelo e os valores são obtidos atraves do objeto stickyNote)
+  // Criar uma nova estrutura de dados para salvar no banco
+  // Atenção!! Os atributos da estrutura precisam ser idênticos ao modelo e os valores são obtidos atraves do objeto stickyNote
   const newNote = noteModel({
     texto: stickyNote.textNote,
     cor: stickyNote.colorNote
   })
+  // Salvar a nota no banco de dados ( Fluxo - Passo 3)
   newNote.save()
+
+  // Enviar ao renderizador um pedido para limpar os campos e setar o formulário com os padrões originais(foco na caixa de texto), usando o preload.js 
+  event.reply('reset-form')
 })
 
-// == Fim - CRUD Create ==============================================================================
-// ===================================================================================================
+// ============== FIM CRUD CREATE ================  
+// ===============================================
--- preload.js
+++ preload.js
@@ -1,21 +1,23 @@
 /**
- * preload.js - Usado no framework electron para aumentar a segurança e o desempenho
+ *  preload.js - Usado no framework electron para aumentar a segurança e o desempenho
  */
 
-// importação dos recursos do framework electron
-// ipcRenderer permite estabelecer uma comunicação entre processos (IPC) main.js <=> renderer.js
+// Importação dos recursos do framework electron
+// ipcRenderer permite estabelecer uma comunicação entre processos (IPC) mains.js - renderer.js
 // contextBridge: permissões de comunicação entre processos usando a api do electron
-const { ipcRenderer, contextBridge } = require('electron')
+const {ipcRenderer, contextBridge} = require('electron')
 
-//Enviar uma mensagem para o main.js estabelecer uma conexão com o banco de dados quando iniciar a aplicação
-//send (enviar)
-//db-connect (rótulo para identificar a mensagem)
+// Enviar uma mensagem para o main.js estabelecer uma conexão com o banco de dados quando iniciar a aplicação
+// send (enviar)
+// db-connect (rótulo para identificar a mensagem)
 ipcRenderer.send('db-connect')
 
-//permissões para estabelecer a comunicação entre processos
+
+// permissões para estabelecer a comunicação entre processos
 contextBridge.exposeInMainWorld('api', {
     dbStatus: (message) => ipcRenderer.on('db-status', message),
     aboutExit: () => ipcRenderer.send('about-exit'),
-    createNote: (stickynote) => ipcRenderer.send('create-note', stickynote)
+    createNote: (stickyNote) => ipcRenderer.send('create-note', stickyNote),
+    // para mandar um argumento vazio, utilizar args(obrigatório no electron)
+    resetForm: (args) => ipcRenderer.on('reset-form', args)
 })
-
